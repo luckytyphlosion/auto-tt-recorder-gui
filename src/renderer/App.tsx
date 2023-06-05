5566807df7e349b08777cf2fd76771e081d6d2c0
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2023-06-05 10:49:40 -0400

    Temp commit to quickly update GUI

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -241,19 +241,7 @@ class App extends React.Component<AppProps, AppState> {
           </ul>
         </div>
         <form onSubmit={this.handleSubmit}>
-          <div>
-            <label htmlFor="iso-filename">ISO or WBFS: </label>
-            <input type="text"
-              id="iso-filename" name="iso-filename" value={this.state["iso-filename"]}
-              /*onChange={(event) => {(this.onInputChange2("iso-filename", false))}}*/
-              onChange={this.onInputChange} required={true}
-            ></input>
-            <button onClick={event => {
-              this.queueOpenDialog(event, [
-                {name: "ISO/WBFS files", extensions: ["iso", "wbfs"]}
-              ], "iso-filename");
-            }} id="iso-filename-btn" type="button">Browse&#8230;</button>
-          </div>
+
           <div>
             <label htmlFor="chadsoft-ghost-page">Chadsoft ghost page link: </label>
             <input type="text"
---
+++ src/renderer/components/AutoTTRecConfigForm.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+function AutoTTRecConfigForm() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
