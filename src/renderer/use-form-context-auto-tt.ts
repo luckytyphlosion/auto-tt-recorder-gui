9f2c8bb7086b28114046261500234c9de74dab74
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2023-06-24 11:24:09 -0400

    Add useWatchAutoTT.

--- src/renderer/use-form-context-auto-tt.ts
+++ src/renderer/use-form-context-auto-tt.ts
@@ -1,7 +1,11 @@
-import { useFormContext } from "react-hook-form";
+import { useFormContext, useWatch, UseWatchProps } from "react-hook-form";
 
 import { AutoTTRecConfigFormFieldTypes } from "./components/AutoTTRecConfigForm";
 
 export function useFormContextAutoTT() {
   return useFormContext<AutoTTRecConfigFormFieldTypes>();
+}
+
+export function useWatchAutoTT(props: object) {
+  return useWatch<AutoTTRecConfigFormFieldTypes>(props);
 }
