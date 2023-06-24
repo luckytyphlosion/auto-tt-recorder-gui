75e4ab88e19791c3f90ad6b67edd9518ca45f9c0
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2023-06-24 11:31:53 -0400

    Revert 'Use useWatch in BackgroundMusicSourceInput'

--- src/renderer/components/form_components/BackgroundMusicSourceInput.tsx
+++ src/renderer/components/form_components/BackgroundMusicSourceInput.tsx
@@ -1,27 +1,31 @@
 import React, { useState } from "react";
-import { useFormContextAutoTT, useWatchAutoTT } from "../../use-form-context-auto-tt";
-import { useWatch } from "react-hook-form";
+import { useFormContextAutoTT } from "../../use-form-context-auto-tt";
 import { MusicFilenameInput } from "./MusicFilenameInput";
 import { GameVolumeInput } from "./GameVolumeInput";
 import { MusicVolumeInput } from "./MusicVolumeInput";
 
 import useRenderCounter from "../../RenderCounter";
-import { AutoTTRecConfigFormFieldTypes } from "../AutoTTRecConfigForm";
 
 export type BackgroundMusicSource = "music-filename" | "game-bgm" | "none";
 
 export function BackgroundMusicSourceInput() {
   const {register, getValues} = useFormContextAutoTT();
+  function isFormBackgroundMusicFromFilename() {
+    return getValues("background-music-source") === "music-filename";
+  }
 
-  //const backgroundMusicSource = useWatch<AutoTTRecConfigFormFieldTypes>({name: "background-music-source"});
-  const musicFilenameInputEnable = useWatchAutoTT({name: "background-music-source"}) === "music-filename";
+  const [musicFilenameInputEnable, setMusicFilenameInputEnable] = useState(isFormBackgroundMusicFromFilename());
   const renderCounter = useRenderCounter();
+
+  function updateMusicFilenameInputEnable(event: React.MouseEvent<HTMLButtonElement>) {
+    setMusicFilenameInputEnable(isFormBackgroundMusicFromFilename());
+  }
 
   return (
     <div>
       <label htmlFor="background-music-source">Background Music: </label>
       <select {...register("background-music-source", {
-        required: true})}>
+        required: false, onChange: updateMusicFilenameInputEnable})}>
         <option value="music-filename">Music filename</option>
         <option value="game-bgm">Game BGM</option>
         <option value="none">None</option>
