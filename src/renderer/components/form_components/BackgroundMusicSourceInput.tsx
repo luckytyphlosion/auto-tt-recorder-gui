e58333e2a6310908a7b25757460d9a96fdb9adca
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2023-06-24 11:24:23 -0400

    Use useWatch in BackgroundMusicSourceInput

--- src/renderer/components/form_components/BackgroundMusicSourceInput.tsx
+++ src/renderer/components/form_components/BackgroundMusicSourceInput.tsx
@@ -1,42 +1,38 @@
 import React, { useState } from "react";
-import { useFormContextAutoTT } from "../../use-form-context-auto-tt";
+import { useFormContextAutoTT, useWatchAutoTT } from "../../use-form-context-auto-tt";
+import { useWatch } from "react-hook-form";
 import { MusicFilenameInput } from "./MusicFilenameInput";
 import { GameVolumeInput } from "./GameVolumeInput";
 import { MusicVolumeInput } from "./MusicVolumeInput";
 
 import useRenderCounter from "../../RenderCounter";
+import { AutoTTRecConfigFormFieldTypes } from "../AutoTTRecConfigForm";
 
 export type BackgroundMusicSource = "music-filename" | "game-bgm" | "none";
 
 export function BackgroundMusicSourceInput() {
   const {register, getValues} = useFormContextAutoTT();
-  function isFormBackgroundMusicFromFilename() {
-    return getValues("background-music-source") === "music-filename";
-  }
 
-  const [musicFilenameInputEnable, setMusicFilenameInputEnable] = useState(isFormBackgroundMusicFromFilename());
+  //const backgroundMusicSource = useWatch<AutoTTRecConfigFormFieldTypes>({name: "background-music-source"});
+  const backgroundMusicSource = useWatchAutoTT({name: "background-music-source"});
   const renderCounter = useRenderCounter();
-
-  function updateMusicFilenameInputEnable(event: React.MouseEvent<HTMLButtonElement>) {
-    setMusicFilenameInputEnable(isFormBackgroundMusicFromFilename());
-  }
 
   return (
     <div>
       <label htmlFor="background-music-source">Background Music: </label>
       <select {...register("background-music-source", {
-        required: false, onChange: updateMusicFilenameInputEnable})}>
+        required: true})}>
         <option value="music-filename">Music filename</option>
         <option value="game-bgm">Game BGM</option>
         <option value="none">None</option>
       </select>
       {
-        musicFilenameInputEnable ? 
+        backgroundMusicSource === "music-filename" ? 
          <MusicFilenameInput/> : ""
       }
       {renderCounter}
       {
-        musicFilenameInputEnable ?
+        backgroundMusicSource === "music-filename" ?
         <>
           <GameVolumeInput/>
           <MusicVolumeInput/>
