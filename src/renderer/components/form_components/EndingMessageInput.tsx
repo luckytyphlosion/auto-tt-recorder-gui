50a35e581b2d29871f9e170020b11c1adce1b013
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2023-08-22 13:23:57 -0400

    Late validate ending-message.

--- src/renderer/components/form_components/EndingMessageInput.tsx
+++ src/renderer/components/form_components/EndingMessageInput.tsx
@@ -1,27 +1,9 @@
 import React from "react";
-import { useFormContextAutoTT } from "../../use-form-context-auto-tt";
-import { SimpleErrorMessage } from "../SimpleErrorMessage";
-
-import useRenderCounter from "../../RenderCounter";
+import { TextInput } from "../TextInput";
 
 //props: {register: UseFormRegister<AutoTTRecConfigFormFields>}
 export function EndingMessageInput() {
-  const {register} = useFormContextAutoTT();
-
-  const renderCounter = useRenderCounter(false, "EndingMessageInput");
-
   return (
-    <div className="grid-contents">
-      <label className="start-label" htmlFor="ending-message">Ending message:</label>
-      <div className="start-label-contents">
-        <input type="text" id="ending-message" {...register("ending-message", {required: {
-            value: true,
-            message: "This input is required."
-          }})}
-        ></input>
-        <SimpleErrorMessage name="ending-message"/>
-        {renderCounter}
-      </div>
-    </div>
+    <TextInput name="ending-message" startLabel="Ending message:"/>
   );
 }
